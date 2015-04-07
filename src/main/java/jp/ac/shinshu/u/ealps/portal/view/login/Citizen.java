ab520868d0a95b5161076dc6d9f07c4bde88ec39
OsamuHasegawa <good.old.memories.quoll@gmail.com>
2015-04-08 05:45:42 +0900

    市民用のログインページを追加

---
+++ src/main/java/jp/ac/shinshu/u/ealps/portal/view/login/Citizen.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package jp.ac.shinshu.u.ealps.portal.view.login;
+
+import jp.ac.shinshu.u.ealps.portal.view.EALPSPortalWebPage;
+
+/**
+ * @author Osamu HASEGAWA
+ *
+ */
+public class Citizen extends EALPSPortalWebPage {
+
+	private static final long serialVersionUID = 5205056644436015115L;
+
+	public Citizen() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
