d8543cc5f1d3d97f4e65fe2381277b727a494adb
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2015-09-30 13:41:33 +0900

    市民のログインページでuidが取得できていなかった問題を修正

--- src/main/java/jp/ac/shinshu/u/ealps/portal/view/login/Citizen.java
+++ src/main/java/jp/ac/shinshu/u/ealps/portal/view/login/Citizen.java
@@ -3,6 +3,13 @@
  */
 package jp.ac.shinshu.u.ealps.portal.view.login;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import jp.ac.shinshu.u.ealps.portal.panel.InformationPanel;
 import jp.ac.shinshu.u.ealps.portal.panel.SchedulePanel;
 import jp.ac.shinshu.u.ealps.portal.service.IUtilityService;
@@ -11,6 +18,8 @@ import jp.ac.shinshu.u.ealps.portal.view.EALPSPortalWebPage;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.protocol.http.servlet.ServletWebRequest;
+import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import com.google.inject.Inject;
@@ -22,22 +31,46 @@ import com.google.inject.Inject;
 public class Citizen extends EALPSPortalWebPage {
 
 	private static final long serialVersionUID = 5205056644436015115L;
-	
+
 	@Inject
 	private IUtilityService UtilityService;
 
 	public Citizen(final PageParameters parameters) {
-		
-		titleModel.setObject("市民開放講座 時間割｜eALPSポータル");
-		
-		String uid = getRequest().getRequestParameters().getParameterValue("uid").toString();
-		String j_username = getRequest().getRequestParameters().getParameterValue("j_username").toString();
-		String userId = getRequest().getRequestParameters().getParameterValue("userId").toString();
-		
+
+titleModel.setObject("市民開放講座 時間割｜eALPSポータル");
+
+		HttpServletRequest httpServletRequest = ((ServletWebRequest) RequestCycle.get().getRequest()).getContainerRequest();
+
+		// ヘッダー確認用
+		@SuppressWarnings("unchecked")
+		Enumeration<String> headerNames = httpServletRequest.getHeaderNames();
+		StringBuilder headerName = new StringBuilder("");
+		if(headerNames != null) {
+			while( headerNames.hasMoreElements()) {
+				headerName.append(headerNames.nextElement());
+				headerName.append(", ");
+			}
+		}
+
+		String uid = httpServletRequest.getHeader("uid") == null ? "" : httpServletRequest.getHeader("uid").toString();
+		String j_username = httpServletRequest.getHeader("j_username") == null ? "" : httpServletRequest.getHeader("j_username").toString();
+		String userId = httpServletRequest.getHeader("userId") == null ? "" : httpServletRequest.getHeader("userId").toString();
+
+//		String uid = httpServletRequest.getAttribute("uid") == null ? "" : httpServletRequest.getAttribute("uid").toString();
+//		String j_username = httpServletRequest.getAttribute("j_username") == null ? "" : httpServletRequest.getAttribute("j_username").toString();
+//		String userId = httpServletRequest.getAttribute("userId") == null ? "" : httpServletRequest.getAttribute("userId").toString();
+
+//		String uid = getRequest().getRequestParameters().getParameterValue("uid").toString();
+//		String j_username = getRequest().getRequestParameters().getParameterValue("j_username").toString();
+//		String userId = getRequest().getRequestParameters().getParameterValue("userId").toString();
+
+		// ヘッダー確認用
+		this.add(new Label("headerNames", Model.of(headerNames.toString())));
+
 		this.add(new Label("j_username", Model.of(j_username)));
 		this.add(new Label("userId", Model.of(userId)));
 		this.add(new Label("uid", Model.of(uid)));
-		
+
 		if(StringUtils.isEmpty(uid)) {
 			uid = "";
 		}
