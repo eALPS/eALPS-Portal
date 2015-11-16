c1269c7c7775c12470afef4f999085430b0ce6b8
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2015-11-16 15:45:09 +0900

    eALPSを利用するうえでの注意事項をパネル化

---
+++ src/main/java/jp/ac/shinshu/u/ealps/portal/panel/AttentionPanel.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+package jp.ac.shinshu.u.ealps.portal.panel;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * @author Osamu HASEGAWA
+ *
+ */
+public class AttentionPanel extends Panel {
+
+	private static final long serialVersionUID = 8158419776238997245L;
+
+	/**
+	 * @param id
+	 */
+	public AttentionPanel(String id) {
+		super(id);
+		drow();
+	}
+
+	private void drow() {
+
+	}
+
+}
