f369c54a54e09300df629cec4554329ff8d941f3
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2016-04-08 21:16:17 +0900

    表示する年度を制御するサービスを実装 ・3月21日以前は表示する年を前年とする

--- src/main/java/jp/ac/shinshu/u/ealps/portal/service/IUtilityService.java
+++ src/main/java/jp/ac/shinshu/u/ealps/portal/service/IUtilityService.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package jp.ac.shinshu.u.ealps.portal.service;
 
@@ -14,18 +14,23 @@ import com.google.inject.ImplementedBy;
  */
 @ImplementedBy(UtilityService.class)
 public interface IUtilityService {
-	
+
 	/**
-	 * 
 	 * <p>初期表示用の年度を取得する．</p>
-	 * <p>運用上の問題で3月20日以降は次年度とする．</p>
-	 * 
+	 * <p>運用上の問題で3月22日以降は次年度とする．</p>
+	 *
 	 * @return	成功：int initYear
 	 * 			失敗：0
 	 */
 	public int getScheduleInitYear();
-	
-	
-	public List<Integer> getYearList(int year);
+
+	/**
+	 * <p>現在の年から5年分の年リストを取得する．</p>
+	 * <p>ただし，最小の年は2015とする</p>
+	 *
+	 * @return	成功：List<Integer> yearList
+	 * 			失敗：空のリスト
+	 */
+	public List<Integer> getYearList();
 
 }
--- src/main/java/jp/ac/shinshu/u/ealps/portal/service/UtilityService.java
+++ src/main/java/jp/ac/shinshu/u/ealps/portal/service/UtilityService.java
@@ -4,6 +4,7 @@
 package jp.ac.shinshu.u.ealps.portal.service;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -21,43 +22,33 @@ public class UtilityService implements IUtilityService {
 
 	@Override
 	public int getScheduleInitYear() {
-//		GregorianCalendar gcal = new GregorianCalendar();
-//		int year = gcal.get(GregorianCalendar.YEAR);
-//		int month = gcal.get(GregorianCalendar.MONTH);
-//		int day = gcal.get(GregorianCalendar.DAY_OF_MONTH);
-//
-//		if (month < 4) {
-//			if (month == 3 && day > 19) {
-//				return year;
-//			} else {
-//				return year-1;
-//			}
-//		} else {
-//			return year;
-//		}
-		return 2015;
+		GregorianCalendar gcal = new GregorianCalendar();
+		int year = gcal.get(GregorianCalendar.YEAR);
+		int month = gcal.get(GregorianCalendar.MONTH);
+		int day = gcal.get(GregorianCalendar.DAY_OF_MONTH);
+
+		if (month <= 2) {
+			if (month == 2 && day > 22) {
+				return year;
+			} else {
+				return year-1;
+			}
+		} else {
+			return year;
+		}
 	}
 
 	@Override
-	public List<Integer> getYearList(int year) {
+	public List<Integer> getYearList() {
 		List<Integer> yearList = new ArrayList<Integer>();
-//		GregorianCalendar gcal = new GregorianCalendar();
-//		int year = gcal.get(GregorianCalendar.YEAR);
-//		int month = gcal.get(GregorianCalendar.MONTH);
-//		int day = gcal.get(GregorianCalendar.DAY_OF_MONTH);
-//
-//		if (month < 4) {
-//			if (month == 3 && day > 19) {
-//				return year;
-//			} else {
-//				return year-1;
-//			}
-//		} else {
-//			return year;
-//		}
+		GregorianCalendar gcal = new GregorianCalendar();
+		int year = gcal.get(GregorianCalendar.YEAR);
 
 		for (int i = 0; i < 5; i++) {
 			yearList.add(year-i);
+			if(year-i <= 2015 ) {
+				break;
+			}
 		}
 
 		return yearList;
