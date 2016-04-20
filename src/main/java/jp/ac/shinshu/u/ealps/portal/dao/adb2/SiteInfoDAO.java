a1e092b183c59798b21f41c2fdf3255301284f20
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2016-04-20 11:00:55 +0900

    運用しているサイト情報取得用のエンティティとDAOを追加

---
+++ src/main/java/jp/ac/shinshu/u/ealps/portal/dao/adb2/SiteInfoDAO.java
@@ -0,0 +1,72 @@
+/**
+ *
+ */
+package jp.ac.shinshu.u.ealps.portal.dao.adb2;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.ac.shinshu.u.common.JDBCUtility;
+import jp.ac.shinshu.u.ealps.portal.entity.SiteInfo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>お知らせテーブル用のDAO</p>
+ *
+ * @version 1.0.0
+ * @author Osamu HASEGAWA
+ *
+ */
+public class SiteInfoDAO extends ADB2AbstractDAO {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	/**
+	 * <p>デフォルトコンストラクタ．</p>
+	 */
+	public SiteInfoDAO() {
+		super();
+	}
+
+	public List<SiteInfo> selectSiteInfoList(int year) {
+		List<SiteInfo> siteInfoList = new ArrayList<SiteInfo>();
+
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT a.siteInfoId, a.siteUid, a.opYear, a.dispOrder, a.visible, a.caption, a.domainName, a.token ");
+		sql.append("FROM siteInfo a ");
+		sql.append("WHERE a.opYear = ? ");
+		sql.append("AND a.dispOrder IS NOT NULL ");
+		sql.append("ORDER BY a.dispOrder");
+//		sql.append("AND CURRENT_TIMESTAMP BETWEEN a.open AND a.close ");
+		ResultSet resultSet = find(sql.toString(), year);
+
+		try {
+			SiteInfo siteInfo = null;
+			while(resultSet.next()) {
+				siteInfo = new SiteInfo();
+				siteInfo.setSiteInfoId(resultSet.getInt("siteInfoId"));
+				siteInfo.setSiteUId(resultSet.getString("siteUid"));
+				siteInfo.setOpYear(resultSet.getInt("opYear"));
+				siteInfo.setDispOrder(resultSet.getInt("dispOrder"));
+				siteInfo.setVisible(resultSet.getInt("visible"));
+				siteInfo.setCaption(resultSet.getString("caption"));
+				siteInfo.setDomainName(resultSet.getString("domainName"));
+				siteInfo.setToken(resultSet.getString("token"));
+
+				siteInfoList.add(siteInfo);
+			}
+		} catch (SQLException e) {
+			logger.error("SQLException", e);
+			throw new RuntimeException(e);
+		} finally {
+			JDBCUtility.close(statement, resultSet);
+		}
+
+		return siteInfoList;
+	}
+
+}
---
+++ src/main/java/jp/ac/shinshu/u/ealps/portal/entity/SiteInfo.java
@@ -0,0 +1,73 @@
+/**
+ *
+ */
+package jp.ac.shinshu.u.ealps.portal.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author Osamu HASEGAWA
+ *
+ */
+public class SiteInfo implements Serializable {
+
+	private static final long serialVersionUID = -6638541549278760081L;
+	private int siteInfoId;
+	private String siteUId;
+	private int opYear;
+	private int dispOrder;
+	private int visible;
+	private String caption;
+	private String domainName;
+	private String token;
+
+	public int getSiteInfoId() {
+		return siteInfoId;
+	}
+	public void setSiteInfoId(int siteInfoId) {
+		this.siteInfoId = siteInfoId;
+	}
+	public String getSiteUId() {
+		return siteUId;
+	}
+	public void setSiteUId(String siteUId) {
+		this.siteUId = siteUId;
+	}
+	public int getOpYear() {
+		return opYear;
+	}
+	public void setOpYear(int opYear) {
+		this.opYear = opYear;
+	}
+	public int getDispOrder() {
+		return dispOrder;
+	}
+	public void setDispOrder(int dispOrder) {
+		this.dispOrder = dispOrder;
+	}
+	public int getVisible() {
+		return visible;
+	}
+	public void setVisible(int visible) {
+		this.visible = visible;
+	}
+	public String getCaption() {
+		return caption;
+	}
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+	public String getDomainName() {
+		return domainName;
+	}
+	public void setDomainName(String domainName) {
+		this.domainName = domainName;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
