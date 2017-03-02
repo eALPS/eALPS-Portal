4483748c3556fa78487ddb4e796c210824d99680
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2017-03-02 16:41:16 +0900

    Summary：eALPSを利用する上での注意事項のパネル化
    New
    ・なし
    
    Modified
    ・eALPSを利用する上での注意事項の文面を変更
    
    Fixed
    ・なし

--- src/main/java/jp/ac/shinshu/u/ealps/portal/panel/AttentionPanel.java
+++ src/main/java/jp/ac/shinshu/u/ealps/portal/panel/AttentionPanel.java
@@ -1,9 +1,8 @@
-/**
- *
- */
 package jp.ac.shinshu.u.ealps.portal.panel;
 
+import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
 
 /**
  * @author Osamu HASEGAWA
@@ -14,14 +13,18 @@ public class AttentionPanel extends Panel {
 	private static final long serialVersionUID = 8158419776238997245L;
 
 	/**
-	 * @param id
+	 * @param id panelId
 	 */
 	public AttentionPanel(String id) {
 		super(id);
-		drow();
+		draw();
 	}
 
-	private void drow() {
+	private void draw() {
+
+		//IModel href = new Modle<String>("https://docs.google.com/forms/d/e/1FAIpQLSdnJaVW-GgY5dpNeFg0uDJMIRurtPyK-wCCWZuOa8L6fl9Njg/viewform");
+
+		add(new ExternalLink("eLearningCenterForm", Model.of("https://docs.google.com/forms/d/e/1FAIpQLSdnJaVW-GgY5dpNeFg0uDJMIRurtPyK-wCCWZuOa8L6fl9Njg/viewform"), Model.of("e-Learningセンター")));
 
 	}
 
