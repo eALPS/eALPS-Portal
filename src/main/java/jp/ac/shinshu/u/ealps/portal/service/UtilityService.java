f663102a6a6b978af32d10bee3528e1ab2e1e9fa
Masaaki Niimura <niimura@cs.shinshu-u.ac.jp>
2019-03-26 09:09:58 +0900

    年度切り替えの日付指定の修正
    New
    ・年度切り替え日かを判定するプライベートメソッドの追加
    
    Modified
    ・年度デフォルト値を返す関数を，切替日判定メソッドを使うよう変更
    ・年度リストの長さを，年度切替日前は 4，それ以外は 3 にするよう変更
    
    Fixed
    ・なし

--- src/main/java/jp/ac/shinshu/u/ealps/portal/service/UtilityService.java
+++ src/main/java/jp/ac/shinshu/u/ealps/portal/service/UtilityService.java
@@ -20,6 +20,23 @@ public class UtilityService implements IUtilityService {
 
 	}
 
+	/**
+	 * 年度切り替え日以前か？
+	 *
+	 *
+	 * @return 新年度に表示を切り替える以前であれば true (ex 2月1日とか)
+	 */
+	private boolean isBeforeNewSchoolYear(int month, int day) {
+		// month は 0 から
+		if ( (month < 2) || (month == 2 && day < 20) ) {
+			return true;
+		} else {
+			return false;
+		}
+
+	};
+
+
 	@Override
 	public int getScheduleInitYear() {
 		GregorianCalendar gcal = new GregorianCalendar();
@@ -27,28 +44,31 @@ public class UtilityService implements IUtilityService {
 		int month = gcal.get(GregorianCalendar.MONTH);
 		int day = gcal.get(GregorianCalendar.DAY_OF_MONTH);
 
-		if (month <= 2) {
-			if (month == 2 && day >= 22) {
-				return year;
-			} else {
-				return year-1;
-			}
+		if ( isBeforeNewSchoolYear(month, day) ) {
+			return year-1;
 		} else {
 			return year;
 		}
 	}
+
 
 	@Override
 	public List<Integer> getYearList() {
 		List<Integer> yearList = new ArrayList<Integer>();
 		GregorianCalendar gcal = new GregorianCalendar();
 		int year = gcal.get(GregorianCalendar.YEAR);
+		int month = gcal.get(GregorianCalendar.MONTH);
+		int day = gcal.get(GregorianCalendar.DAY_OF_MONTH);
 
-		for (int i = 0; i < 5; i++) {
+		// Yearリストの表示の長さ　デフォルトは 3
+		int showYearLength = 3;
+		//  年度切り替え前 は長さ 4
+		if ( isBeforeNewSchoolYear(month, day) ) {
+			showYearLength = 4;
+		}
+
+		for (int i = 0; i < showYearLength; i++) {
 			yearList.add(year-i);
-			if(year-i <= 2016 ) {
-				break;
-			}
 		}
 
 		return yearList;
